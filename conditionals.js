ad7693bc3700e0a8c0eb7ef954aa96629ab665c1
developer-bot-hub <117781817+developer-bot-hub@users.noreply.github.com>
2023-01-10 00:39:47 -0300

    Conditionals
    Find the hidden message

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+if (10 < 20) {
+    document.write("bigger ")
+}
+if (2 === 2) {
+    document.write("just ")
+}
+if (true === true) {
+    document.write("epic true ")
+}
+let youAreLookingForADeveloper = "iAmADeveloper";
+if (youAreLookingForADeveloper == "iAmADeveloper") {
+    document.write("it's me, come on!");
+}
